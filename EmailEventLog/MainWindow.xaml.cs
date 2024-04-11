d09d909626cf6083045c55da99d35b76cc0fa07b
Terry Holmes <tchaser3@outlook.com>
2024-04-11 07:11:05 -0400

    Add project files.

---
+++ EmailEventLog/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using NewEventLogDLL;
+
+namespace EmailEventLog
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //setting up the classes
+        WPFMessagesClass TheMessagesClass = new WPFMessagesClass();
+        EventLogClass TheEventLogClass = new EventLogClass();
+        SendEmailClass TheSendEmailClass = new SendEmailClass();
+
+        //getting the data
+        FindEventLogEntriesForEmailDataSet TheFindEventLogEntriesForEmailDataSet = new FindEventLogEntriesForEmailDataSet();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            int intCounter;
+            int intNumberOfRecords;
+            string strEventID;
+            string strEventDate;
+            string strLogEntry;
+            string strMessage;
+
+            try
+            {
+                TheFindEventLogEntriesForEmailDataSet = TheEventLogClass.FindEventLogEntriesForEmail();
+
+                intNumberOfRecords = TheFindEventLogEntriesForEmailDataSet.FindEventLogEntriesForEmail.Rows.Count;
+
+                strMessage = "<h1>Event Log Entries For the last 15 Days</h1>";
+                strMessage += "<table><tr><th>Event ID </th><th>Event Date</th><th>Log Entry</th></tr>";
+
+                if(intNumberOfRecords > 0)
+                {
+                    for (intCounter = 0; intCounter < intNumberOfRecords; intCounter++)
+                    {
+                        strEventID = Convert.ToString(TheFindEventLogEntriesForEmailDataSet.FindEventLogEntriesForEmail[intCounter].EventID);
+                        strEventDate = Convert.ToString(TheFindEventLogEntriesForEmailDataSet.FindEventLogEntriesForEmail[intCounter].EventDate);
+                        strLogEntry = TheFindEventLogEntriesForEmailDataSet.FindEventLogEntriesForEmail[intCounter].LogEntry;
+
+                        strMessage += "<tr><th>" + strEventID + "</th>";
+                        strMessage += "<th>" + strEventDate + "</th>";
+                        strMessage += "<th>" + strLogEntry + "</th></tr>";
+                    }
+                }
+
+                strMessage += "</table>";
+
+                TheSendEmailClass.SendEventLogReport(strMessage);
+
+                Application.Current.Shutdown();
+            }
+            catch (Exception Ex)
+            {
+                TheSendEmailClass.SendEventLog(Ex.ToString());
+            }
+
+        }
+    }
+}
---
+++ EmailEventLog/SendEmailClass.cs
@@ -0,0 +1,117 @@
+﻿/* Title:           Send Email Class
+ * Date:            7-12-17
+ * Author:          Terry Holmes */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Mail;
+using NewEventLogDLL;
+using System.Configuration;
+using System.Net;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using System.IO;
+
+
+namespace EmailEventLog
+{
+    class SendEmailClass
+    {
+        //setting up the classes
+        WPFMessagesClass TheMessagesClass = new WPFMessagesClass();
+        EventLogClass TheEventLogClass = new EventLogClass();
+
+        public void SendEventLog(string strLogEntry)
+        {
+            string strComputerName;
+            string strUser;
+            string strMessage;
+            bool blnFatalError = false;
+            string strHeader = "Event Log Entry Report";
+            string strEmailAddress = "itadmin@bluejaycommunications.com";
+
+            try
+            {
+                strComputerName = System.Environment.MachineName;
+                strUser = System.Environment.UserName;
+
+                strMessage = "<h1>" + strUser + " " + strComputerName + " " + strLogEntry + "</h1>";
+
+                blnFatalError = !(SendEmail(strEmailAddress, strHeader, strMessage));
+
+                if (blnFatalError == true)
+                    throw new Exception();
+            }
+            catch (Exception Ex)
+            {
+                TheEventLogClass.InsertEventLogEntry(DateTime.Now, "New Blue Jay ERP // Send Email Class // Send Event Log " + Ex.Message);
+
+                TheMessagesClass.ErrorMessage(Ex.ToString());
+            }
+
+        }
+        public void SendEventLogReport(string strMessage)
+        {
+            string strComputerName;
+            string strUser;
+            bool blnFatalError = false;
+            string strHeader = "Event Log Entry Report";
+            string strEmailAddress = "tholmes@bluejaycommunications.com";
+
+            try
+            {
+                blnFatalError = !(SendEmail(strEmailAddress, strHeader, strMessage));
+
+                if (blnFatalError == true)
+                    throw new Exception();
+            }
+            catch (Exception Ex)
+            {
+                TheEventLogClass.InsertEventLogEntry(DateTime.Now, "New Blue Jay ERP // Send Email Class // Send Event Log " + Ex.Message);
+
+                TheMessagesClass.ErrorMessage(Ex.ToString());
+            }
+
+        }
+        public bool SendEmail(string mailTo, string subject, string message)
+        {
+            try
+            {
+
+                MailMessage mailMessage = new MailMessage("EventLogReports@bluejaycommunications.com", mailTo, subject, message);
+                mailMessage.IsBodyHtml = true;
+                mailMessage.BodyEncoding = Encoding.UTF8;
+                mailMessage.SubjectEncoding = Encoding.UTF8;
+
+                SmtpClient smtpClient = new SmtpClient("192.168.0.240", 25);
+                smtpClient.UseDefaultCredentials = false;
+                smtpClient.EnableSsl = false;
+                smtpClient.DeliveryMethod = System.Net.Mail.SmtpDeliveryMethod.Network;
+                smtpClient.Send(mailMessage);
+                return true;
+            }
+            catch (Exception Ex)
+            {
+                TheEventLogClass.InsertEventLogEntry(DateTime.Now, "Blue Jay ERP // Send Email Class // Send Mail " + Ex.Message);
+
+                TheMessagesClass.ErrorMessage(Ex.ToString());
+
+                return false;
+            }
+
+
+        }
+    }
+}
